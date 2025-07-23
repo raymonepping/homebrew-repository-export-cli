bb2fd4235410e299c27b1e56d7a4dfaf99cf4c2a
Raymon Epping <your@email.com>
2025-07-23 16:00:23 +0200

    🔖 repository-export-cli: release v1.0.5

--- Formula/repository-export-cli.rb
+++ Formula/repository-export-cli.rb
@@ -1,10 +1,10 @@
 class RepositoryExportCli < Formula
   desc "Export, document, and manage your GitHub repositories with a single CLI"
   homepage "https://github.com/raymonepping/homebrew-repository-export-cli"
-  url "https://github.com/raymonepping/homebrew-repository-export-cli/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "4dee7b539d7634c22e629e9ef5d39825baf83bd4f31d2ceb8c4f92b4a5db906f"
+  url "https://github.com/raymonepping/homebrew-repository-export-cli/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "05be6a60dab4bd12433cc0aaac45ec92b87b845b08cf944bc26fa1f0fef776f0"
   license "MIT"
-  version "1.0.4"
+  version "1.0.5"
 
   depends_on "bash"
   depends_on "jq"
